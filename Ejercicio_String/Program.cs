fa10b3eddd25271e9d40b58152d1d31c999a3a5a
Michelle Fabiola Martinez Yebra <dayebra@hotmail.com>
2021-09-27 02:21:17 -0700

    Agregar archivos de proyecto.

---
+++ Ejercicio_String/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nombre, ciudad;
+            Console.WriteLine("Escribe tu nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Escribe tu ciudad");
+            ciudad = Console.ReadLine();
+            Console.WriteLine("Hola \n" + nombre + " Bienvenido a " + ciudad);
+        }
+    }
+}
